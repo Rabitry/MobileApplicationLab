3fa7636bbc0c96bd8961392dc9d63f3c13688751
Rabitry <rabitryhasda.ru2020@gmail.com>
2023-08-05 12:18:25 +0600

    first commit

---
+++ hello_world/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyScaffold extends StatelessWidget {
+  const MyScaffold({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece
+    // of paper on which the UI appears.
+    return Material(
+      // Column is a vertical, linear layout.
+            child: Center(
+              child: Text('Hello world!'),
+            ),
+    );
+  }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'My app', // used by the OS task switcher
+      home: SafeArea(
+        child: MyScaffold(),
+      ),
+    ),
+  );
+}
